2aabffbcdeb0b8d0ff1687dae2b2da4d3b6e86c9
marxolly <mark@solly.com.au>
2021-04-22 12:06:23 +1000

    Update addPurchaseOrder.php

--- app/views/purchaseorders/addPurchaseOrder.php
+++ app/views/purchaseorders/addPurchaseOrder.php
@@ -5,5 +5,35 @@
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+        <form id="add_purchase_order" method="post" action="/form/procAddPurchaseOrder">
+            <div class="row">
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+<!-------------------------------------------------     Purchase Order Details     ---------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+                <div class="col-sm-12 col-md-6 col-lg-5 mb-3" id="podetails">
+                    <div class="card h-100 border-secondary po-card">
+                        <div class="card-header bg-secondary text-white">
+                            Purchase Order Details
+                        </div>
+                        <div class="card-body">
+                            The Details Go Here
+                        </div>
+                    </div>
+                </div>
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+<!-------------------------------------------------     Purchase Order Items     ------------------------------------------------------------>
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+               <div class="col-sm-12 col-md-6 col-lg-7 mb-3" id="poitems">
+                    <div class="card h-100 border-secondary po-card">
+                        <div class="card-header bg-secondary text-white">
+                            Purchase Order Items
+                        </div>
+                        <div class="card-body">
+                            The Items Go Here
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
