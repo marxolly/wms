3c781f3a03411018adbaa656e1d507873e9a8744
marxolly <mark@solly.com.au>
2021-04-28 09:24:28 +1000

    Update common.js

--- public_html/scripts/common.js
+++ public_html/scripts/common.js
@@ -4,7 +4,7 @@ Refresh Page if no activity and show a countdown
 var time = new Date().getTime();
 var refresh_rate = 300000; //milliseconds
 $('form').attr('autocomplete','off');
-refresh();
+//refresh();
 $(document).ready(function(e){
     console.log( "ready!" );
     $(document).bind("mousemove keypress", function(e) {
