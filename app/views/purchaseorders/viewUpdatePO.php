ffb1803ec6b1c5607e8a4f2e2095e835da5c4224
marxolly <mark@solly.com.au>
2021-05-05 15:58:15 +1000

    po erro checks

--- app/views/purchaseorders/viewUpdatePO.php
+++ app/views/purchaseorders/viewUpdatePO.php
@@ -4,8 +4,15 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
-        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
-        <?php echo "<pre>FORM VALUES",print_r(Form::$values),"</pre>"; ?>
-
+        <?php if($error):?>
+            <?php if($error == "no_po_id"):
+                include(Config::get('VIEWS_PATH')."layout/page-includes/no_po_id.php");
+            elseif($error == "no_po"):
+                include(Config::get('VIEWS_PATH')."layout/page-includes/no_po_found.php");
+            endif;?>
+        <?php else:?>
+            <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+            <?php echo "<pre>FORM VALUES",print_r(Form::$values),"</pre>"; ?>
+        <?php endif;?>
     </div>
 </div>
