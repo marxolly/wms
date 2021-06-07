10f057f960b9b7a5f3d74f8be17e7e1b0e68d24a
marxolly <mark@solly.com.au>
2021-06-08 09:25:06 +1000

    Update StylesController.php

--- app/controllers/StylesController.php
+++ app/controllers/StylesController.php
@@ -16,7 +16,8 @@ class stylesController extends Controller
 
     public function clientStyling()
     {
-        $client_id =  0;
+        $user_role = (Session::isAdminUser())? 'admin' : Session::getUserRole();
+        $client_id = ($user_role == 'client')? $this->user->getUserClientId( Session::getUserId() ) : 0;
         $style_defaults = array(
             'logo'                      => '<img width="130" src="/images/FSG_logo@130px.png" class="custom-logo" alt="FSG" style="display:none;" title="WMS Home" /><img width="130" src="/images/FSG_logo_white@130px.png" class="custom-logo-transparent" alt="FSG logo" title="WMS Home" />',
             'card_border_colour'        => '#545380',
@@ -26,13 +27,9 @@ class stylesController extends Controller
         );
         $client_styles = $this->displaystyle->getClientStyles($client_id);
         $styles = (empty($client_styles))? $style_defaults: array_merge($style_defaults, $client_styles);
-        $data = array(
-            'client_id' => $client_id
-        );
-        $data = array_merge($data, $styles);
         //render the page
         Config::setJsConfig('curPage', "shipto-reps");
-        $this->view->renderStyleSheet(Config::get('VIEWS_PATH') . "stylesheets/website-style.php",$data);
+        $this->view->renderStyleSheet(Config::get('VIEWS_PATH') . "stylesheets/website-style.php",$styles);
     }
 
     public function isAuthorized()
