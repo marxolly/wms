7508c61db879ef5fd110e6a906b97dea05790097
marxolly <mark@solly.com.au>
2021-06-08 10:59:59 +1000

    Update StylesController.php

--- app/controllers/StylesController.php
+++ app/controllers/StylesController.php
@@ -21,9 +21,10 @@ class stylesController extends Controller
         $style_defaults = array(
             'logo'                      => '<img width="130" src="/images/FSG_logo@130px.png" class="custom-logo" alt="FSG" style="display:none;" title="WMS Home" /><img width="130" src="/images/FSG_logo_white@130px.png" class="custom-logo-transparent" alt="FSG logo" title="WMS Home" />',
             'card_border_colour'        => '#545380',
-            'card_header_colour'        => '#4183C2',
-            'card_header_background'    => 'rgba(180,195,220,0.1)',
-            'card_header_border_colour' => '#545380'
+            'card_header_colour'        => '#242359',
+            'card_header_background'    => '#b4c3dc',
+            'card_header_border_colour' => '#545380',
+            'quicklinks_header_colour'  => '#17142c'
         );
         $client_styles = $this->displaystyle->getClientStyles($client_id);
         $styles = (empty($client_styles))? $style_defaults: array_merge($style_defaults, $client_styles);
