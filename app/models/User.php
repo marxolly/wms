6dd1421e1074bf4a9d7513cdb49a447a4dd3a93a
marxolly <mark@solly.com.au>
2021-06-09 14:36:20 +1000

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -76,7 +76,7 @@ class User extends Model{
 
     public function updateProfileInfo($data, $userId)
     {
-        echo "updating profile<pre>",print_r($data),"</pre>"; die();
+        //echo "updating profile<pre>",print_r($data),"</pre>"; die();
         $db = Database::openConnection();
         $vals = array(
             'name'  =>  $data['name']
