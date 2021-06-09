af0dac5cc3fe3e1c244c95b4ec573c82e48d3694
marxolly <mark@solly.com.au>
2021-06-09 16:09:23 +1000

    client admin controller

---
+++ app/controllers/DisplaysettingsController.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Display Settings - client admins only controller
+ *
+
+ * @author     Mark Solly <mark.solly@fsg.com.au>
+ */
+
+class DisplaySettingsController extends Controller
+{
+    public function beforeAction()
+    {
+        parent::beforeAction();
+    }
+
+
+    public function index()
+    {
+        //set the page name for menu display
+        Config::setJsConfig('curPage', 'display-settings-index');
+        parent::displayIndex(get_class());
+    }
+
+    public function isAuthorized(){
+        $role = Session::getUserRole();
+        $action = $this->request->param('action');
+        $resource = "displaysettings";
+        // only for super admins
+        Permission::allow('client admin', $resource, ['*']);
+        return Permission::check($role, $resource, $action);
+    }
+}
+?>
