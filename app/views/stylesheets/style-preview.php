b62db146bf74709dbfb2f3256d7ecf63156f91c7
marxolly <mark@solly.com.au>
2021-06-11 09:33:12 +1000

    Update style-preview.php

--- app/views/stylesheets/style-preview.php
+++ app/views/stylesheets/style-preview.php
@@ -6,15 +6,30 @@ div#page_header_preview{
     background: <?php echo $top_banner_background;?>;
     width:75%;
     margin: 1em auto;
-}
-div#page_header_preview h1{
     color: #fff;
     text-align:center
+}
+div#page_header_preview h1{
+}
+h2.page-header_preview {
+    font-size: 40px;
+    letter-spacing: 3.5px;
+    font-weight: 800;
+    text-transform: uppercase;
+    text-align: center;
+    margin-top: 25px;
+    color: <?php echo $page_header_colour;?>;
 }
 
 </style>
 <div id="page_header_preview" class="row">
     <div class="col-lg-12">
-        <h1>PAGE TOP BANNER</h1>
+        <h1>Website Header Colour</h1>
+        <p>The background is the Top Banner Background</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        <h2 class="page-header_preview">page title colour</h2>
     </div>
 </div>
