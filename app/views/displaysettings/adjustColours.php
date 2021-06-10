fcad500ef59814343c1baa5087c4bafaec6f28e4
marxolly <mark@solly.com.au>
2021-06-10 12:59:49 +1000

    Update adjustColours.php

--- app/views/displaysettings/adjustColours.php
+++ app/views/displaysettings/adjustColours.php
@@ -4,5 +4,15 @@
 <div id="page-wrapper">
     <div id="page_container" class="container-xl">
         <?php include(Config::get('VIEWS_PATH')."layout/page-includes/page_top.php");?>
+        <?php include(Config::get('VIEWS_PATH')."layout/page-includes/form-top.php");?>
+        <?php echo Form::displayError('general');?>
+        <form id="adjust_colours"  method="post" enctype="multipart/form-data" action="/form/procAdjustColours">
+            <input type="hidden" name="csrf_token" value="<?php echo Session::generateCsrfToken(); ?>" />
+            <div class="form-group row">
+                <div class="col-md-4 offset-col-md3">
+                    <button type="submit" class="btn btn-outline-fsg">Save Changes</button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
