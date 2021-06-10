d26bb245f93693591c545c06155a76f97baf7c4c
marxolly <mark@solly.com.au>
2021-06-10 11:29:23 +1000

    Update StylesController.php

--- app/controllers/StylesController.php
+++ app/controllers/StylesController.php
@@ -28,7 +28,7 @@ class stylesController extends Controller
             'fsg_button_colour_hover'   => '#242359',
             'page_header_colour'        => '#242359',
             'quicklinks_header_colour'  => '#17142c',
-            'top_banner_background'     => 'linear-gradient(90deg, rgba(138,105,140,1) 0%, rgba(115,99,139,1) 15%, rgba(84,83,128,1) 50%, rgba(65,131,194,1) 85%, rgba(130,165,208,1) 100%)'
+            'top_banner_background'     => 'linear-gradient(161deg, rgba(138,105,140,1) 0%, rgba(36,35,89,1) 25%, rgba(23,20,44,1) 40%, rgba(23,20,44,1) 70%, rgba(65,131,194,1) 90%, rgba(130,165,208,1) 100%);'
         );
         $client_styles = $this->displaystyle->getClientStyles($client_id);
         if(!empty($client_styles))
