e9021f68b997d9813d3653d56e28d3bc901637ee
marxolly <mark@solly.com.au>
2021-06-15 14:40:29 +1000

    Update adjustColours.php

--- app/views/displaysettings/adjustColours.php
+++ app/views/displaysettings/adjustColours.php
@@ -17,8 +17,6 @@
             <div class="col-lg-12">
                 <?php if(isset($_SESSION['feedback'])) :?>
                    <div class='feedbackbox'><?php echo Session::getAndDestroy('feedback');?></div>
-                <?php if(isset($_SESSION['errorfeedback'])) :?>
-                   <div class='errorbox'><?php echo Session::getAndDestroy('errorfeedback');?></div>
                 <?php endif; ?>
             </div>
         </div>
