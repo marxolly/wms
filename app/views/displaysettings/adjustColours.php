972425727456529a015d136533d68eb31f2d4d04
marxolly <mark@solly.com.au>
2021-06-23 15:44:37 +1000

    Update adjustColours.php

--- app/views/displaysettings/adjustColours.php
+++ app/views/displaysettings/adjustColours.php
@@ -65,7 +65,7 @@
                 </div>
                 <div class="col-md-4">
                     <label class="col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Colour</label>
-                    <div class="colour-picker input-group mb-3" id="bgc">
+                    <div class="colour-picker input-group mb-3">
                         <input type="text" class="form-control required colour" name="card_header_background" id="card_header_background" data-defaultvalue="<?php echo STYLE_DEFAULTS['card_header_background'];?>" value="<?php echo $styles['card_header_background'];?>" >
                         <div class="input-group-append">
                             <span class="input-group-text colorpicker-input-addon"><i></i></span>
@@ -77,6 +77,31 @@
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input defaultbox" data-section="card_header_background" id="default_1" name="default_1" />
                         <label class="custom-control-label" for="default_1"></label>
+                    </div>
+                </div>
+            </div>
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+<!-------------------------------------------------     Card Header Colour      ---------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------------------------------->
+            <div class="p-3 m-2 border rounded row">
+                <div class="col-md-5" style="margin: auto 1px">
+                    <h4>Card Header Text Colour</h4>
+                    <p class="inst">This also sets the text colour in the card body</p>
+                </div>
+                <div class="col-md-4">
+                    <label class="col-form-label"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Colour</label>
+                    <div class="colour-picker input-group mb-3">
+                        <input type="text" class="form-control required colour" name="card_header_colour" id="card_header_colour" data-defaultvalue="<?php echo STYLE_DEFAULTS['card_header_colour'];?>" value="<?php echo $styles['card_header_colour'];?>" >
+                        <div class="input-group-append">
+                            <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 text-right">
+                    <label class="col-form-label" for="default_1">Use Default</label>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input defaultbox" data-section="card_header_colour" id="default_2" name="default_2" />
+                        <label class="custom-control-label" for="default_2"></label>
                     </div>
                 </div>
             </div>
