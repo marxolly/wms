4e64d4590934956df25fed02c0bff3408ebe0e25
marxolly <mark@solly.com.au>
2021-06-24 15:04:12 +1000

    Update footer_scripts.php

--- app/views/layout/page-includes/footer_scripts.php
+++ app/views/layout/page-includes/footer_scripts.php
@@ -64,5 +64,7 @@
         <script src="/scripts/common.js?t=<?php echo time();?>"></script>
         <!-- FontAwesome Pro Kit -->
         <script src="https://kit.fontawesome.com/cc79da085a.js" crossorigin="anonymous"></script>
+        <!-- Bootstrap Colour Picker -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-colorpicker/3.2.0/js/bootstrap-colorpicker.min.js" integrity="sha512-INXxqXxcP6zawSei7i47Xmu+6ZIBRbsYN1LHAy5H1gzl1XIfTbI/OLjUcvBnDD8F3ZSVB6mf8asEPTMxz4VNjw==" crossorigin="anonymous"></script>
         <!-- Spectrum Colour Picker -->
         <script src="https://cdn.jsdelivr.net/npm/spectrum-colorpicker2/dist/spectrum.min.js"></script>
