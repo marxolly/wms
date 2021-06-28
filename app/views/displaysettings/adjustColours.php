bb9543cf3c2241ef1fb7289782eeb8baf9fcf05c
marxolly <mark@solly.com.au>
2021-06-28 16:21:06 +1000

    Update adjustColours.php

--- app/views/displaysettings/adjustColours.php
+++ app/views/displaysettings/adjustColours.php
@@ -158,12 +158,12 @@
                     </div>
                 </div>
     <!------------------------------------------------------------------------------------------------------------------------------------------->
-    <!-------------------------------------------------     FSG Button Hover Text Colour      ---------------------------------------------------------------->
+    <!-------------------------------------------------     FSG Button Hover Colour      ---------------------------------------------------------------->
     <!------------------------------------------------------------------------------------------------------------------------------------------->
                 <div class="col-sm-12 col-md-6 p-3 mb-2">
                     <div class="card h-100 adjustcolour-card">
                         <div class="card-header">
-                            <h4>Button Hover Text Colour</h4>
+                            <h4>Button Hover Colour</h4>
                         </div>
                         <div class="card-body">
                             <div class="form-group row">
@@ -172,11 +172,11 @@
                             <div class="form-group row">
                                 <label class="col-md-4"><sup><small><i class="fas fa-asterisk text-danger"></i></small></sup> Colour</label>
                                 <div class="col-md-8">
-                                    <input type="text" class="form-control required colour-picker" name="fsg_button_hover_text_colour" id="fsg_button_hover_text_colour" data-defaultvalue="<?php echo STYLE_DEFAULTS['fsg_button_hover_text_colour'];?>" value="<?php echo $styles['fsg_button_hover_text_colour'];?>" >
+                                    <input type="text" class="form-control required colour-picker" name="fsg_button_colour_hover" id="fsg_button_colour_hover" data-defaultvalue="<?php echo STYLE_DEFAULTS['fsg_button_colour_hover'];?>" value="<?php echo $styles['fsg_button_colour_hover'];?>" >
                                 </div>
                             </div>
                             <div class="form-group row custom-control custom-checkbox custom-control-right">
-                                <input type="checkbox" class="custom-control-input defaultbox" data-section="fsg_button_hover_text_colour" id="default_5" name="default_5" />
+                                <input type="checkbox" class="custom-control-input defaultbox" data-section="fsg_button_colour_hover" id="default_5" name="default_5" />
                                 <label class="custom-control-label col-md-5" for="default_5">Use Default</label>
                             </div>
                         </div>
